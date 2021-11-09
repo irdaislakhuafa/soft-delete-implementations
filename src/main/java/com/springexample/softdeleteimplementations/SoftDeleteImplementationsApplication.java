44523f307e557a4103538952251c9f5d0bca5517
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2021-11-10 00:11:57 +0700

    First commit

---
+++ src/main/java/com/springexample/softdeleteimplementations/SoftDeleteImplementationsApplication.java
@@ -0,0 +1,13 @@
+package com.springexample.softdeleteimplementations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftDeleteImplementationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftDeleteImplementationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springexample/softdeleteimplementations/SoftDeleteImplementationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springexample.softdeleteimplementations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftDeleteImplementationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
